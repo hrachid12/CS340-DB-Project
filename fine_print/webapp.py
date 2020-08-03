816e853cee657c13b2757a9160cf32f4dce3c508
Hassan <hassan.rachid5@gmail.com>
2020-08-03 14:40:29 -0400

    Add select & insert for products & orders_products

--- fine_print/webapp.py
+++ fine_print/webapp.py
@@ -28,6 +28,10 @@ def orders():
         customerID = request.form['customerID']
         couponID = request.form['couponID']
 
+        # If no coupon is selected
+        if couponID == 'NULL':
+            couponID = None
+
         # connect to database
         db_connection = connect_to_database()
 
@@ -43,10 +47,10 @@ def orders():
     result = execute_query(db_connection, query).fetchall()
 
     query = 'SELECT customer_id, fname, lname FROM customers'
-    customerResult = execute_query(db_connection, query)
+    customerResult = execute_query(db_connection, query).fetchall()
 
     query = 'SELECT coupon_id, promotion FROM coupons'
-    couponResult = execute_query(db_connection, query)
+    couponResult = execute_query(db_connection, query).fetchall()
 
     print(result)
     return render_template('orders.html', rows=result, customers=customerResult, coupons=couponResult)
@@ -56,7 +60,27 @@ def orders():
 
 @app.route("/products", methods=['GET', 'POST'])
 def products():
-    return render_template("products.html")
+    db_connection = connect_to_database()
+
+    if request.method == 'POST':
+
+        # Fetch data from the form
+        itemName = request.form['itemName']
+        price = request.form['price']
+        quantityAvailable = request.form['quantityAvailable']
+
+        # Build query string and execute
+        query = 'INSERT INTO products (item_name, price, quantity_available) VALUES (%s, %s, %s)'
+        data = (itemName, price, quantityAvailable)
+        execute_query(db_connection, query, data)
+
+    print('Fetching info from database')
+
+    # Create query strings and execute to product table data
+    query = 'SELECT * FROM products'
+    result = execute_query(db_connection, query).fetchall()
+
+    return render_template("products.html", rows=result)
 
 # Customers
 
@@ -77,7 +101,24 @@ def coupons():
 
 @app.route("/orders_products", methods=['GET', 'POST'])
 def orders_products():
-    return render_template("orders_products.html")
+
+    # Connect to database
+    db_connection = connect_to_database()
+
+    if request.method == "POST":
+        pass
+
+    # Build query selections for intersection table, order table, and product, table
+    query = 'SELECT * FROM orders_products'
+    result = execute_query(db_connection, query)
+
+    query = 'SELECT order_id, fname, lname, order_date FROM orders LEFT JOIN customers ON orders.customer_id = customers.customer_id'
+    orders = execute_query(db_connection, query)
+
+    query = 'SELECT product_id, item_name FROM products'
+    products = execute_query(db_connection, query)
+
+    return render_template("orders_products.html", rows=result, orders=orders, products=products)
 
 
 # Coupons_Customers Intersection Table
