67292723d1a48171cfcf2d08d77d37903338cbe1
Hassan <hassan.rachid5@gmail.com>
2020-08-03 12:08:31 -0400

    reorganized package structure, added db connection

---
+++ db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_rachidh'
+passwd = 'HRachid12'
+db = 'cs340_rachidh'
---
+++ run.py
@@ -0,0 +1,4 @@
+from fine_print import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
