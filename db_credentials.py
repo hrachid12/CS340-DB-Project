e70c88d9ea73970b3500b3819c48b7587f70ba7b
Hassan <hassan.rachid5@gmail.com>
2020-08-05 13:46:57 -0400

    switched db used & removed extra column from cust

--- db_credentials.py
+++ db_credentials.py
@@ -1,14 +1,14 @@
 # Credentials for OSU Database
 
-# host = 'classmysql.engr.oregonstate.edu'
-# user = 'cs340_rachidh'
-# password = 'HRachid12'
-# db = 'cs340_rachidh'
+host = 'classmysql.engr.oregonstate.edu'
+user = 'cs340_rachidh'
+password = 'HRachid12'
+db = 'cs340_rachidh'
 
 
 # Credentials for other database
 
-host = 'sql9.freemysqlhosting.net'
-user = 'sql9358519'
-password = 'JRfs89PGVS'
-db = 'sql9358519'
+# host = 'sql9.freemysqlhosting.net'
+# user = 'sql9358519'
+# password = 'JRfs89PGVS'
+# db = 'sql9358519'
