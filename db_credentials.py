075351e29cdc9d899c054ac595baff442a5ac601
Hassan <hassan.rachid5@gmail.com>
2020-09-02 20:50:49 -0400

    updated db credentials

--- db_credentials.py
+++ db_credentials.py
@@ -1,14 +1,14 @@
 # Credentials for OSU Database
 
-host = 'classmysql.engr.oregonstate.edu'
-user = 'cs340_rachidh'
-password = 'HRachid12'
-db = 'cs340_rachidh'
+# host = 'classmysql.engr.oregonstate.edu'
+# user = 'cs340_rachidh'
+# password = 'HRachid12'
+# db = 'cs340_rachidh'
 
 
 # Credentials for other database
 
-# host = 'sql9.freemysqlhosting.net'
-# user = 'sql9358519'
-# password = 'JRfs89PGVS'
-# db = 'sql9358519'
+host = 'ec2-54-86-57-171.compute-1.amazonaws.com'
+user = 'mhpvfkpltsblpr'
+password = '07a059efa88aaad9d8cbf99589498d06d26ee2a835487da679324053a1f3877d'
+db = 'd74mnte24s7mkg'
