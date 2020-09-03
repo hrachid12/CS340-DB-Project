0bf9cb6348b3cf1b0d3cd9dd9383d780d1d64c47
Hassan <hassan.rachid5@gmail.com>
2020-09-02 22:09:24 -0400

    added config file

--- fine_print/__init__.py
+++ fine_print/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from fine_print.config import Config
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     from fine_print.main.routes import main 
 
--- fine_print/config.py
+++ fine_print/config.py
@@ -2,4 +2,5 @@ import os
 
 
 class Config:
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
